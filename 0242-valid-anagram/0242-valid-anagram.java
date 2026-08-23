1472db43409c8789a6443956ebaa0a728da6e231
Maduraa BS <maduraakrishna@gmail.com>
2026-08-23 13:51:37 +0530

    Time: 5 ms (62.07%), Space: 44.1 MB (95.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        //if the lengths are diff, they cannot be anagrams
         if(s.length() != t.length()){
             return false;
         }
-        int[] count = new int[26];
+        //creating an array to count character frequencies
+        int[] count = new int[26]; //assuming only lowercase English characters
         
         for(int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            count[s.charAt(i) - 'a']++; //whenever we find a character in 's',increase the array count by 1
+            count[t.charAt(i) - 'a']--;//for every character present in t, decrement the array count by 1
         }
+        //check if all the counts are zero ie 0
         for(int i=0; i<26; i++){
             if(count[i] != 0){
                 return false;
