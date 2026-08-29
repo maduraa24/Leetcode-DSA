2806bb6df30e7196bc09c816a26846bdc850e04e
Maduraa BS <maduraakrishna@gmail.com>
2026-08-29 19:26:26 +0530

    Time: 5 ms (32.12%), Space: 46.7 MB (37.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,38 +1,36 @@
 class Solution {
-    static Map<String, Integer> values = new HashMap<>();
-    static {
-        values.put("I", 1);
-        values.put("V", 5);
-        values.put("X", 10);
-        values.put("L", 50);
-        values.put("C", 100);
-        values.put("D", 500);
-        values.put("M", 1000);
-        values.put("IV", 4);
-        values.put("IX", 9);
-        values.put("XL", 40);
-        values.put("XC", 90);
-        values.put("CD", 400);
-        values.put("CM", 900);
-    }
-    public int romanToInt(String s) {
-       int sum = 0;
-        int i = 0;
-        
-        while(i<s.length()){
-            if(i<s.length()-1){
-                String twoSymbols = s.substring(i,i+2);
-                if(values.containsKey(twoSymbols)){
-                    sum+= values.get(twoSymbols);
-                    i = i+2;
-                    continue;
-                }
-            }
-            
-            String oneSymbol = s.substring(i, i+1);
-            sum +=  values.get(oneSymbol);
-            i = i+1;
+        static Map <String,Integer> valuesHash=new HashMap<>();
+        static{
+            valuesHash.put("I",1);
+            valuesHash.put("V", 5);
+            valuesHash.put("X", 10);
+            valuesHash.put("L", 50);
+            valuesHash.put("C", 100);
+            valuesHash.put("D", 500);
+            valuesHash.put("M", 1000);
+            valuesHash.put("IV", 4);
+            valuesHash.put("IX", 9);
+            valuesHash.put("XL", 40);
+            valuesHash.put("XC", 90);
+            valuesHash.put("CD", 400);
+            valuesHash.put("CM", 900);
         }
-        return sum; 
+        public int romanToInt(String s){
+            int sum=0,i=0;
+
+            while(i<s.length()){
+                if(i<s.length()-1){
+                    String twoSymbols=s.substring(i,i+2);
+                    if(valuesHash.containsKey(twoSymbols)){
+                        sum+=valuesHash.get(twoSymbols);
+                        i=i+2;
+                        continue;
+                    }
+                }
+                String oneSymbol=s.substring(i,i+1);
+                sum+=valuesHash.get(oneSymbol);
+                i++;
+            }
+            return sum;
+        }
     }
-}
