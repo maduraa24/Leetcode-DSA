da62b35bea3091bdb06bfdcf1b9149fefec6574a
Maduraa BS <maduraakrishna@gmail.com>
2026-09-08 20:32:35 +0530

    Time: 2 ms (35.46%), Space: 71.2 MB (59.51%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,43 +3,42 @@ class Solution {
         int n = nums.length;
         int contains = 0;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 1){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
                 contains++;
                 break;
             }
         }
-        if(contains == 0){
+        if (contains == 0) {
             return 1;
         }
-        
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] <= 0 || nums[i] > n){
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
                 nums[i] = 1;
             }
         }
-        
-        for(int i=0; i<nums.length; i++){
-            int a= Math.abs(nums[i]);
-            
-            if(a==n){
+
+        for (int i = 0; i < nums.length; i++) {
+            int a = Math.abs(nums[i]);
+
+            if (a == n) {
                 nums[0] = -Math.abs(nums[0]);
-            }
-            else{
+            } else {
                 nums[a] = -Math.abs(nums[a]);
             }
         }
-  
-        for(int i = 1 ; i< n; i++){
-            if(nums[i] > 0){
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
                 return i;
             }
         }
-  
-        if(nums[0] > 0){
+
+        if (nums[0] > 0) {
             return n;
         }
-     
-        return n+1;
+
+        return n + 1;
     }
 }
