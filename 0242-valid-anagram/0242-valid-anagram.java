4aa25fc21d11d3270c049ebc983a86d0a4974dc8
Maduraa BS <maduraakrishna@gmail.com>
2026-09-11 19:49:57 +0530

    Time: 5 ms (63.03%), Space: 44.3 MB (82.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //if the lengths are diff, they cannot be anagrams
-        if(s.length() != t.length()){
+       //if th lengths are diff, they cannot be anaagrams
+       if(s.length()!=t.length()){
+        return false;
+       } 
+       //create an array to count the frequencies
+       int[] count=new int[26]; //english 26 alphabets
+
+       for(int i=0;i<s.length();i++){
+        count[s.charAt(i)-'a']++;
+        //for every character in s, increase its character ie alphabet array count by 1;
+        count[t.charAt(i)-'a']--;
+        //for every character in t, decrease its character ie alphabet array count by 1;
+       }
+       //check if all the count has become 0 again
+       for(int i=0;i<26;i++){
+        if(count[i]!=0){
             return false;
         }
-        //creating an array to count character frequencies
-        int[] count = new int[26]; //assuming only lowercase English characters
-        
-        for(int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++; //whenever we find a character in 's',increase the array count by 1
-            count[t.charAt(i) - 'a']--;//for every character present in t, decrement the array count by 1
-        }
-        //check if all the counts are zero ie 0
-        for(int i=0; i<26; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-        return true;
-        
+       }
+       return true;
     }
-  }
+}
