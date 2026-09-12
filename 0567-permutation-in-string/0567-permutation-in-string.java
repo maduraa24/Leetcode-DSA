b0979a9868076e2f3d81a7e647cfaeaed8d1aace
Maduraa BS <maduraakrishna@gmail.com>
2026-09-12 19:03:30 +0530

    Time: 8 ms (49.59%), Space: 43.8 MB (85.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,63 +1,37 @@
 class Solution {
+public boolean checkInclusion(String s1, String s2) {
+    if (s1.length() > s2.length()) {
+        return false;
+}
+    int[] s1Map = new int[26];
+    int[] s2Map = new int[26];
 
-    public boolean checkInclusion(String s1, String s2) {
+    // Initialize frequency maps for s1 and the first window of s2
+    for (int i = 0; i < s1.length(); i++) {
+        s1Map[s1.charAt(i) - 'a']++;
+        s2Map[s2.charAt(i) - 'a']++;
+    }
 
-        // If s1 is bigger than s2, permutation cannot exist
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
-        // Frequency map of s1
-        HashMap<Character, Integer> s1Map = new HashMap<>();
-
-        // Frequency map of current window in s2
-        HashMap<Character, Integer> s2Map = new HashMap<>();
-
-        // Build both frequency maps
-        for (int i = 0; i < s1.length(); i++) {
-
-            // Increase frequency of current character in s1
-            s1Map.put(s1.charAt(i),
-                    s1Map.getOrDefault(s1.charAt(i), 0) + 1);
-
-            // Increase frequency of current character in first window of s2
-            s2Map.put(s2.charAt(i),
-                    s2Map.getOrDefault(s2.charAt(i), 0) + 1);
-        }
-
-        // If first window itself is a permutation
-        if (s1Map.equals(s2Map)) {
+    // Slide the window through s2 and compare the maps
+    for (int i = 0; i < s2.length() - s1.length(); i++) {
+        if (matches(s1Map, s2Map)) {
             return true;
         }
-
-        // Slide the window one character at a time
-        for (int i = s1.length(); i < s2.length(); i++) {
-
-            // Character entering the window
-            char add = s2.charAt(i);
-
-            // Increase its frequency
-            s2Map.put(add,
-                    s2Map.getOrDefault(add, 0) + 1);
-
-            // Character leaving the window
-            char remove = s2.charAt(i - s1.length());
-
-            // Decrease its frequency
-            s2Map.put(remove, s2Map.get(remove) - 1);
-
-            // Remove it completely if frequency becomes 0
-            if (s2Map.get(remove) == 0) {
-                s2Map.remove(remove);
-            }
-
-            // Compare the two maps
-            if (s1Map.equals(s2Map)) {
-                return true;
-            }
-        }
-
-        // No permutation found
-        return false;
+        s2Map[s2.charAt(i + s1.length()) - 'a']++; // Add new character to the window
+        s2Map[s2.charAt(i) - 'a']--; // Remove old character from the window
     }
+
+    // Check the last window
+    return matches(s1Map, s2Map);
+}
+
+// Helper function to compare two frequency maps
+private boolean matches(int[] s1Map, int[] s2Map) {
+    for (int i = 0; i < 26; i++) {
+        if (s1Map[i] != s2Map[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 }
